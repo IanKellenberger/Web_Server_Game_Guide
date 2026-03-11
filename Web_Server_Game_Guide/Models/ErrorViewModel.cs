e785dceeb03a8a2478a755aaf8d5db6971e44d99
Ian <ianmkellenberger@gmail.com>
2026-03-11 13:05:38 -0400

    Add project files.

---
+++ Web_Server_Game_Guide/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web_Server_Game_Guide.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
